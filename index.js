1c6692832a5dc0f1ab3bf96a7486fab945cc8ec1
Tim Macfarlane <tjm@administrate.co>
2018-03-17 10:39:22 +0000

    Test Deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ deploy: 'works' });
 });
 
 const PORT = process.env.PORT || 5000;
