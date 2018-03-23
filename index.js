91e28480e4f98ef908beeb91319e1c37370d9b02
Tim Macfarlane <tjm@administrate.co>
2018-03-23 13:02:21 +0000

    Phase one of google oauth. * Ignoring console logs for now

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+/* eslint-disable no-console */
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send({ deploy: 'works' });
 });
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile) => {
+            console.log('access token', accessToken);
+            console.log('refresh token', refreshToken);
+            console.log('profile', profile);
+        }
+    )
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
